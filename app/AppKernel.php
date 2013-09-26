9274d57292265c6271a8f4ba0a599a7f8dc2abd1
Marius <mzaharie@pitechnologies.ro>
2013-09-26 13:09:51 +0300

    deleted prototype and added FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Bookshop\BookshopBundle\BookshopBookshopBundle(),
             
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
