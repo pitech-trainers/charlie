4113efdb7ec6c58a4b97bc027f69b34efca7e734
Marius <mzaharie@pitechnologies.ro>
2013-09-27 17:44:51 +0300

    registration 80% + login + forgotPassword 50%

--- src/Bookshop/BookshopBundle/Entity/User.php
+++ src/Bookshop/BookshopBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace Bookshop\BookshopBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,10 +19,251 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255, nullable=false)
+     * 
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     */
+    protected $first_name;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255, nullable=false)
+     * 
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     */
+    protected $last_name;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mobile", type="string", length=20, nullable=false)
+     * 
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Regex(
+     *     pattern="/^\d+$/",
+     *     htmlPattern="*",
+     *     match=true,
+     *     message="This mobile number is invalid."
+     * )
+     * @Assert\Regex(
+     *     pattern="/^\d{10,12}$/",
+     *     htmlPattern="*",
+     *     match=true,
+     *     message="This mobile number must contain 10 or 12 digits."
+     * )
+     * 
+     * 
+     */
+    protected $mobile;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="gender", type="string", length=1, nullable=true)
+     */
+    protected $gender;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="education", type="string", length=1, nullable=true)
+     */
+    protected $education;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="billing_address_id", type="integer", nullable=true)
+     */
+    protected $billing_address_id;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="shipping_address_id", type="integer", nullable=true)
+     */
+    protected $shipping_address_id;
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set first_name
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->first_name = $firstName;
+    
+        return $this;
+    }
+
+    /**
+     * Get first_name
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Set last_name
+     *
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+    
+        return $this;
+    }
+
+    /**
+     * Get last_name
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return User
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+    
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string 
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param string $gender
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return string 
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set education
+     *
+     * @param string $education
+     * @return User
+     */
+    public function setEducation($education)
+    {
+        $this->education = $education;
+    
+        return $this;
+    }
+
+    /**
+     * Get education
+     *
+     * @return string 
+     */
+    public function getEducation()
+    {
+        return $this->education;
+    }
+
+    /**
+     * Set billing_address_id
+     *
+     * @param integer $billingAddressId
+     * @return User
+     */
+    public function setBillingAddressId($billingAddressId)
+    {
+        $this->billing_address_id = $billingAddressId;
+    
+        return $this;
+    }
+
+    /**
+     * Get billing_address_id
+     *
+     * @return integer 
+     */
+    public function getBillingAddressId()
+    {
+        return $this->billing_address_id;
+    }
+
+    /**
+     * Set shipping_address_id
+     *
+     * @param integer $shippingAddressId
+     * @return User
+     */
+    public function setShippingAddressId($shippingAddressId)
+    {
+        $this->shipping_address_id = $shippingAddressId;
+    
+        return $this;
+    }
+
+    /**
+     * Get shipping_address_id
+     *
+     * @return integer 
+     */
+    public function getShippingAddressId()
+    {
+        return $this->shipping_address_id;
     }
 }
--- src/Bookshop/BookshopBundle/Menu/Builder.php
+++ src/Bookshop/BookshopBundle/Menu/Builder.php
@@ -1,54 +1,61 @@
 <?php
+
 // src/Acme/DemoBundle/Menu/Builder.php
+
 namespace Bookshop\BookshopBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
-class Builder extends ContainerAware
-{
-//    public function mainMenu(FactoryInterface $factory, array $options)
-//    {
-//        $menu = $factory->createItem('root');
-//        $myTrans = $this->container->get('translator');
-//        
-//        $homeTrans = $myTrans->trans('menu.home', array() ,'AcmeDemoBundle');
-//        $menu->addChild($homeTrans, array('route' => '_welcome'));
-//        $menu->addChild('Demo page', array(
-//            'route' => '_demo'
-////            'routeParameters' => array('id' => 42)
-//        ));
-//        $securityContext = $this->container->get('security.context');
-//        if( $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED') ){
-//            // authenticated REMEMBERED, FULLY will imply REMEMBERED (NON anonymous)
-//            $logoutTrans = $myTrans->trans('layout.logout', array() ,'FOSUserBundle');
-//            $menu->addChild($logoutTrans, array('route' => 'fos_user_security_logout'));
-//            
-//        }else{
-//            $loginTrans = $myTrans->trans('layout.login', array() ,'FOSUserBundle');
-//            $menu->addChild($loginTrans, array('route' => 'fos_user_security_login'));
-//            $rstPassTrans = $this->container->get('translator')->trans('menu.reset_pass', array() ,'AcmeDemoBundle');
-//            $menu->addChild($rstPassTrans, array('route' => 'fos_user_resetting_request'));
+class Builder extends ContainerAware {
+
+    public function linksMenu(FactoryInterface $factory, array $options) {
+        $menu = $factory->createItem('root');
+        $myTrans = $this->container->get('translator');
+
+        $securityContext = $this->container->get('security.context');
+        
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            // authenticated REMEMBERED, FULLY will imply REMEMBERED (NON anonymous)
+            $myAccountTrans = $myTrans->trans('menu.my.account', array() ,'BookshopBundle');
+            $menu->addChild($myAccountTrans, array(
+                'route' => 'bookshop_bookshop_homepage'
+                    )
+            );
+            $myCartTrans = $myTrans->trans('menu.my.cart', array() ,'BookshopBundle');
+            $menu->addChild($myCartTrans, array(
+                'route' => 'bookshop_bookshop_homepage'
+            ));
+            
+            $logoutTrans = $myTrans->trans('layout.logout', array() ,'FOSUserBundle');
+            $menu->addChild($logoutTrans, array('route' => 'fos_user_security_logout'));
+        } else {
+            
+            $loginTrans = $myTrans->trans('layout.login', array() ,'FOSUserBundle');
+            $menu->addChild($loginTrans, array('route' => 'fos_user_security_login'));
+            
+            $rstPassTrans = $myTrans->trans('menu.forgot_pass', array() ,'BookshopBundle');
+            $menu->addChild($rstPassTrans, array('route' => 'fos_user_resetting_request'));
+            
+            $registerTrans = $myTrans->trans('menu.register', array() ,'BookshopBundle');
+            $menu->addChild($registerTrans, array('route' => 'fos_user_registration_register'));
 //            $menu->addChild("Item With Childs", array('route' => '_welcome'));
 //            $menu['Item With Childs']->addChild('first Cild', array('route' => '_welcome'));
 //            $menu['Item With Childs']->addChild('second Cild', array('route' => '_welcome'));
-//        }
-//        
-//        
-//        // ... add more children
-//
-//        return $menu;
-//    }
-    
-    public function categoryMenu(FactoryInterface $factory, array $options){
+        }
+        return $menu;
+    }
+
+    public function categoryMenu(FactoryInterface $factory, array $options) {
         $em = $this->container->get('doctrine.orm.entity_manager');
         $categs = $em->getRepository('BookshopBookshopBundle:Category')->findAll();
         $menu = $factory->createItem('root');
         foreach ($categs as $categ) {
-            $menu->addChild($categ->getName(),  array(
-            'route' => 'category',
-            'routeParameters' => array('id' => $categ->getID())));
+            $menu->addChild($categ->getName(), array(
+                'route' => 'category',
+                'routeParameters' => array('id' => $categ->getID())));
         }
         return $menu;
     }
+
 }
