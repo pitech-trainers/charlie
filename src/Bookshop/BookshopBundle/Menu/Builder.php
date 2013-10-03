4d6c0bf129d1792a02a4806a88cbb112fa6a53d8
Marius <mzaharie@pitechnologies.ro>
2013-10-03 15:51:28 +0300

    dashboardMenu without my orders link

--- src/Bookshop/BookshopBundle/Menu/Builder.php
+++ src/Bookshop/BookshopBundle/Menu/Builder.php
@@ -14,31 +14,30 @@ class Builder extends ContainerAware {
         $myTrans = $this->container->get('translator');
 
         $securityContext = $this->container->get('security.context');
-        
+
         if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             // authenticated REMEMBERED, FULLY will imply REMEMBERED (NON anonymous)
-            $myAccountTrans = $myTrans->trans('menu.my.account', array() ,'BookshopBundle');
+            $myAccountTrans = $myTrans->trans('menu.my.account', array(), 'BookshopBundle');
             $menu->addChild($myAccountTrans, array(
                 'route' => 'dashboard_index'
             ));
-            
-            $myCartTrans = $myTrans->trans('menu.my.cart', array() ,'BookshopBundle');
+
+            $myCartTrans = $myTrans->trans('menu.my.cart', array(), 'BookshopBundle');
             $menu->addChild($myCartTrans, array(
                 'route' => 'bookshop_bookshop_homepage'
             ));
-            
-            $logoutTrans = $myTrans->trans('layout.logout', array() ,'FOSUserBundle');
+
+            $logoutTrans = $myTrans->trans('layout.logout', array(), 'FOSUserBundle');
             $menu->addChild($logoutTrans, array('route' => 'fos_user_security_logout'));
-            
         } else {
-            
-            $loginTrans = $myTrans->trans('layout.login', array() ,'FOSUserBundle');
+
+            $loginTrans = $myTrans->trans('layout.login', array(), 'FOSUserBundle');
             $menu->addChild($loginTrans, array('route' => 'fos_user_security_login'));
-            
-            $rstPassTrans = $myTrans->trans('menu.forgot_pass', array() ,'BookshopBundle');
+
+            $rstPassTrans = $myTrans->trans('menu.forgot_pass', array(), 'BookshopBundle');
             $menu->addChild($rstPassTrans, array('route' => 'fos_user_resetting_request'));
-            
-            $registerTrans = $myTrans->trans('menu.register', array() ,'BookshopBundle');
+
+            $registerTrans = $myTrans->trans('menu.register', array(), 'BookshopBundle');
             $menu->addChild($registerTrans, array('route' => 'fos_user_registration_register'));
 //            $menu->addChild("Item With Childs", array('route' => '_welcome'));
 //            $menu['Item With Childs']->addChild('first Cild', array('route' => '_welcome'));
@@ -56,6 +55,33 @@ class Builder extends ContainerAware {
                 'route' => 'category',
                 'routeParameters' => array('id' => $categ->getID())));
         }
+        return $menu;
+    }
+
+    public function dashboardMenu(FactoryInterface $factory, array $options) {
+        $menu = $factory->createItem('root');
+        $myTrans = $this->container->get('translator');
+
+        $dashHomeTrans = $myTrans->trans('menu.dashboard.home', array(), 'BookshopBundle');
+        $menu->addChild($dashHomeTrans, array(
+            'route' => 'dashboard_index'
+        ));
+        $accInfoTrans = $myTrans->trans('menu.dashboard.account_info', array(), 'BookshopBundle');
+        $menu->addChild($accInfoTrans, array(
+            'route' => 'fos_user_profile_show'
+        ));
+        $myOrdersTrans = $myTrans->trans('menu.dashboard.account_orders', array(), 'BookshopBundle');
+        $menu->addChild($myOrdersTrans, array(
+            'route' => 'dashboard_index'  // not existing feature
+        ));
+        $billingTrans = $myTrans->trans('menu.dashboard.address.billing', array(), 'BookshopBundle');
+        $menu->addChild($billingTrans, array(
+            'route' => 'dashboard_billing_addr_preedit'  // not existing feature
+        ));
+        $shippingTrans = $myTrans->trans('menu.dashboard.address.shipping', array(), 'BookshopBundle');
+        $menu->addChild($shippingTrans, array(
+            'route' => 'dashboard_shipping_addr_preedit'  // not existing feature
+        ));
         return $menu;
     }
 
