0acded46b470ffd946e3a6f2461904f6c8a34383
Andrei Husar <ahusar@pitechnologies.ro>
2013-10-16 17:09:17 +0300

    finished cart

--- src/Bookshop/BookshopBundle/Controller/CheckoutController.php
+++ src/Bookshop/BookshopBundle/Controller/CheckoutController.php
@@ -6,6 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CheckoutController extends Controller {
 
+    public function indexAction() {
+        if (is_null($this->getUser()))
+            $userid = 0;
+        else
+            $userid = $this->getUser()->getID();
+        $em = $this->getDoctrine()->getManager();
+        $cart = $em->getRepository('BookshopBookshopBundle:Cart')->getCart($userid);
+        $cartid = $cart[0]->getId();
+        $cartitems = $em->getRepository('BookshopBookshopBundle:CartItems')->getItems($cartid);
+        if (sizeof($cartitems) == 0) {
+            return $this->render("BookshopBookshopBundle:Error:Empty.html.twig");
+        } else {
+            return $this->redirect($this->generateUrl('billing'));
+        }
+    }
+
     public function billingAction() {
         return $this->render('BookshopBookshopBundle:Checkout:billing.html.twig');
     }
